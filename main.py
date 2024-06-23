f28c2987137095987ecb4bd77704ccf0b05ec93b
Anan-h <anan.hosien@gmail.com>
2024-06-23 14:07:03 +0300

    creating calculator

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+from calculator import Calculator
+
+
 def main():
+    number1=5
+    number2=7
+    sum=Calculator.Sum(number1,number2)
+    sub=Calculator.subtract(number1,number2)
+    mul=Calculator.mulltiply(number1,number2)
+
+    print(f"the summary is :{sum}")
+    print(f"the subtract is :{sub}")
+    print(f"the mulltiply is :{mul}")
 
 main()
